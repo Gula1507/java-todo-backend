e9d7c1db0674aa449883c785bd38c4cc46625753
Aygul <gulya@gmx.net>
2024-11-19 09:32:39 +0100

    add github login and security

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,18 @@ import axios from "axios";
 function App() {
     const [todos, setToDos] = useState<ToDo[]>([])
 
+    function login() {
+        const host = window.location.host === 'localhost:5173' ? 'http://localhost:8080' : window.location.origin
+        window.open(host + '/oauth2/authorization/github', '_self')
+    }
+
+    function getUser() {
+        axios.get("api/users/me")
+            .then(response => {
+                console.log(response.data)
+            })
+    }
+
     useEffect(() => {
         loadAllToDos()
     }, [])
@@ -27,14 +39,15 @@ function App() {
     };
     return (
         <>
+            <button onClick={login}>Login</button>
+            <button onClick={getUser}>Me</button>
             <Header/>
-            <ToDoPage todos={todos} saveToDo={saveToDo} onToDoChanged={loadAllToDos}/>
+            {/*<ToDoPage todos={todos} saveToDo={saveToDo} onToDoChanged={loadAllToDos}/>*/}
 
             <Routes>
-
                 <Route path="/todos" element={<ToDoPage todos={todos} saveToDo={saveToDo}
                                                         onToDoChanged={loadAllToDos}/>}/>
-
+                <Route path="/" element={<p>Homepage</p>}/>
             </Routes>
         </>
     )
